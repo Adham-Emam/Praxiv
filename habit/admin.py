f70c636c9ca5c1b46676de3370e4a00a4cdc375e
Adham Hossam <101532474+Adham-Emam@users.noreply.github.com>
2025-09-26 17:01:56 +0300

    Delete habit directory

--- habit/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Habit
-
-
-# Register your models here.
-@admin.register(Habit)
-class HabitAdmin(admin.ModelAdmin):
-    list_display = ("name", "description", "created_at")
-    search_fields = ("name",)
